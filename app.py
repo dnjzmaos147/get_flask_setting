e397caa5a3291cb477a06f8b75eca43c691636de
wookwook <dnjzmaos147@naver.com>
2021-02-16 16:25:00 +0900

    post_set

--- app.py
+++ app.py
@@ -5,16 +5,30 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
-@app.route('/test')
+@app.route('/testpote')
 def test():
     return render_template('post.html')
 
 @app.route('/pote', methods=['POST'])
-def post():
+def po():
     value = request.form['test']
-    return value
+    return {
+        "d" : "fff",
+        "v" : value
+    }
+
+@app.route('/post', methods=['POST'])
+def post():
+    value = request.form['word']
+    test = "테스트문장"
+    print("val", value)
+    print("test", test)
+    return {
+        "ajax에서받은값" : value,
+        "flaskroute에서주는값" : test
+    }
 
 if __name__ == '__main__':
     app.run()
